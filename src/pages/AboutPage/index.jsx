be4d14645d77255c2957fe2815913ed9586b0d3d
fxxnxx <fxxnxx@gmail.com>
2024-06-18 03:19:43 +0300

    mobile layout

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -27,90 +27,174 @@ const AboutPage = () => {
 
   return (
     <>
-      <Header />
-      <div className={styles.aboutPageWrapper}>
-        <div className={styles.contentWrapper}>
-          <div className={styles.content}>
-            <div className={styles.mainContent}>
-              <div className={styles.phoneWrapper}>
-                {isFirstImage ? (
-                  <img
-                    src={phoneAbout1}
-                    alt="First Image"
-                    className="image aboutPhone"
-                  />
-                ) : (
-                  <img
+    <div className={styles.homePc}>
+        <Header />
+        <div className={styles.aboutPageWrapper}>
+          <div className={styles.contentWrapper}>
+            <div className={styles.content}>
+              <div className={styles.mainContent}>
+                <div className={styles.phoneWrapper}>
+                  {isFirstImage ? (
+                    <img
+                      src={phoneAbout1}
+                      alt="First Image"
+                      className="image aboutPhone"
+                    />
+                  ) : (
+                    <img
                     src={phoneAbout2}
                     alt="Second Image"
                     className="image aboutPhone"
-                  />
-                )}
-              </div>
-              <div className={styles.sideContent}>
-                <div className={styles.switchBtn}>
-                  <img onClick={toggleImage} className="switchBtn" src={switchBtn} alt="switchBtn" />
+                    />
+                  )}
                 </div>
-                <div>
-                  <span className={styles.sideText}>
-                    Видео делает оценки судей максимально  
-                  </span>
-                  <div className={styles.sideText} style={{ color: "#FF3E9B" }}>
-                    объективными!
+                <div className={styles.sideContent}>
+                  <div className={styles.switchBtn}>
+                    <img onClick={toggleImage} className="switchBtn" src={switchBtn} alt="switchBtn" />
+                  </div>
+                  <div>
+                    <span className={styles.sideText}>
+                      Видео делает оценки судей максимально  
+                    </span>
+                    <div className={styles.sideText} style={{ color: "#FF3E9B" }}>
+                      объективными!
+                    </div>
+                  </div>
+                  <div className={styles.heartWrapper}>
+                    <img className={styles.heartDouble} src={heartDouble} alt="heartDouble" />
                   </div>
                 </div>
-                <div className={styles.heartWrapper}>
-                  <img className={styles.heartDouble} src={heartDouble} alt="heartDouble" />
+              </div>
+              <div className={styles.phraseBlock}>
+                <div>
+                  <span className={styles.phraseText}>Несколько отличных</span>
+                  <span className={styles.phraseText} style={{ color: "#FF3E9B" }}> фото</span>
+                </div>
+                <div>
+                  <span className={styles.phraseText} >Не могут показать больше чем одно</span>
+                  <span className={styles.phraseText} style={{ color: "#FF3E9B" }}> видео</span>
                 </div>
               </div>
-            </div>
-            <div className={styles.phraseBlock}>
-              <div>
-                <span className={styles.phraseText}>Несколько отличных</span>
-                <span className={styles.phraseText} style={{ color: "#FF3E9B" }}> фото</span>
+              <div className={styles.likeWrapper}>
+                <img className="like" src={like} alt="like" />
               </div>
-              <div>
-                <span className={styles.phraseText} >Не могут показать больше чем одно</span>
-                <span className={styles.phraseText} style={{ color: "#FF3E9B" }}> видео</span>
-              </div>
-            </div>
-            <div className={styles.likeWrapper}>
-              <img className="like" src={like} alt="like" />
-            </div>
-            <div className={styles.summaryBlock} style={{ width: '926px', height: '400px', padding: '89px 30px', marginTop: 0 }}>
-              <div>
-                <span className={styles.phraseText} style={{ fontSize: '32px' }}>Объективная оценка качества:</span>
-              </div>
-              <div>
-                <span className={styles.phraseText} style={{ fontSize: '32px',  color: "#FF3E9B" }}>BeautyRank </span>
-                <span className={styles.phraseText} style={{ fontSize: '32px' }}> использует продвинутую систему судейства, основанную на объективных критериях и мнениях экспертов. Это гарантирует, что каждый специалист оценивается справедливо и честно.</span>
+              <div className={styles.summaryBlock} style={{ width: '926px', height: '400px', padding: '89px 30px', marginTop: 0 }}>
+                <div>
+                  <span className={styles.phraseText} style={{ fontSize: '32px' }}>Объективная оценка качества:</span>
+                </div>
+                <div>
+                  <span className={styles.phraseText} style={{ fontSize: '32px',  color: "#FF3E9B" }}>BeautyRank </span>
+                  <span className={styles.phraseText} style={{ fontSize: '32px' }}> использует продвинутую систему судейства, основанную на объективных критериях и мнениях экспертов. Это гарантирует, что каждый специалист оценивается справедливо и честно.</span>
+                </div>
               </div>
             </div>
           </div>
+          <img className={styles.leftBackgroundComet} src={BackAboutLeft} alt="img" />
+          <img className={styles.rightBackgroundComet} src={BackAboutRight} alt="img" />
+          <div className={styles.backgroundImgWrapper}>
+            <img
+              className={styles.backgroundImg}
+              src={Background}
+              alt="Background"
+              />
+          </div>
+          <div className={styles.footerBackgroundWrapper}>
+            <img
+              className={styles.footerBackground}
+              src={LayerBottom}
+              alt="Layer_bottom"
+              />
+          </div>
         </div>
-        
-
-        <img className={styles.leftBackgroundComet} src={BackAboutLeft} alt="img" />
-        <img className={styles.rightBackgroundComet} src={BackAboutRight} alt="img" />
-        <div className={styles.backgroundImgWrapper}>
-          <img
-            className={styles.backgroundImg}
-            src={Background}
-            alt="Background"
-          />
-        </div>
-        <div className={styles.footerBackgroundWrapper}>
-          <img
-            className={styles.footerBackground}
-            src={LayerBottom}
-            alt="Layer_bottom"
-          />
-        </div>
+        <footer className={styles.partnersPageFooter}>
+          <img className={styles.footerLogo} src={FooterLogo} alt="" />
+          <button class={styles.contactUsBtn}>Связаться с нами</button>
+        </footer>
       </div>
-      <footer className={styles.partnersPageFooter}>
-        <img className={styles.footerLogo} src={FooterLogo} alt="" />
-        <button class={styles.contactUsBtn}>Связаться с нами</button>
-      </footer>
+      <div className={styles.homeModile}>
+        <Header />
+        <div className={styles.aboutPageWrapper}>
+          <div className={styles.contentWrapper}>
+            <div className={styles.content}>
+              <div className={styles.mainContent}>
+                <div className={styles.phoneWrapper}>
+                  {isFirstImage ? (
+                    <img
+                      src={phoneAbout1}
+                      alt="First Image"
+                      className="image aboutPhone"
+                    />
+                  ) : (
+                    <img
+                    src={phoneAbout2}
+                    alt="Second Image"
+                    className="image aboutPhone"
+                    />
+                  )}
+                </div>
+                <div className={styles.sideContent}>
+                  <div className={styles.switchBtn}>
+                    <img onClick={toggleImage} className="switchBtn" src={switchBtn} alt="switchBtn" />
+                  </div>
+                  <div>
+                    <span className={styles.sideText}>
+                      Видео делает оценки судей максимально  
+                    </span>
+                    <div className={styles.sideText} style={{ color: "#FF3E9B" }}>
+                      объективными!
+                    </div>
+                  </div>
+                  <div className={styles.heartWrapper}>
+                    <img className={styles.heartDouble} src={heartDouble} alt="heartDouble" />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.phraseBlock}>
+                <div>
+                  <span className={styles.phraseText}>Несколько отличных</span>
+                  <span className={styles.phraseText} style={{ color: "#FF3E9B" }}> фото</span>
+                </div>
+                <div>
+                  <span className={styles.phraseText} >Не могут показать больше чем одно</span>
+                  <span className={styles.phraseText} style={{ color: "#FF3E9B" }}> видео</span>
+                </div>
+              </div>
+              <div className={styles.likeWrapper}>
+                <img className="like" src={like} alt="like" />
+              </div>
+              <div className={styles.summaryBlock} style={{ width: '926px', height: '400px', padding: '89px 30px', marginTop: 0 }}>
+                <div>
+                  <span className={styles.phraseText} style={{ fontSize: '32px' }}>Объективная оценка качества:</span>
+                </div>
+                <div>
+                  <span className={styles.phraseText} style={{ fontSize: '32px',  color: "#FF3E9B" }}>BeautyRank </span>
+                  <span className={styles.phraseText} style={{ fontSize: '32px' }}> использует продвинутую систему судейства, основанную на объективных критериях и мнениях экспертов. Это гарантирует, что каждый специалист оценивается справедливо и честно.</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img className={styles.leftBackgroundComet} src={BackAboutLeft} alt="img" />
+          <img className={styles.rightBackgroundComet} src={BackAboutRight} alt="img" />
+          <div className={styles.backgroundImgWrapper}>
+            <img
+              className={styles.backgroundImg}
+              src={Background}
+              alt="Background"
+              />
+          </div>
+          <div className={styles.footerBackgroundWrapper}>
+            <img
+              className={styles.footerBackground}
+              src={LayerBottom}
+              alt="Layer_bottom"
+              />
+          </div>
+        </div>
+        <footer className={styles.partnersPageFooter}>
+          <img className={styles.footerLogo} src={FooterLogo} alt="" />
+          <button class={styles.contactUsBtn}>Связаться с нами</button>
+        </footer>
+      </div>
     </>
   );
 };
